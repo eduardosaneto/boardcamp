f43b453853064e3e1e6890ab5d5585b74b727ef9
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-06-18 16:24:20 -0300

    rotas get, post e delete de aluguel de jogos

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import 'dayjs/locale/pt-br.js';
+import { createRequire } from 'module';
+
 
 import connection from './database/database.js';
 import userSchema from './dataValidation.js';
@@ -9,6 +12,10 @@ const PORT = 4000;
 
 server.use(cors());
 server.use(express.json());
+
+const require = createRequire(import.meta.url);
+const dayjs = require('dayjs');
+let now = dayjs().locale('pt-br').format("YYYY/MM/DD");
 
 //CATEGORIES
 server.get('/categories', async (req, res) => {
@@ -36,7 +43,7 @@ server.post('/categories', async (req, res) => {
     }
 });
 
-//JOGOS
+//GAMES
 server.get('/games', async (req, res) => {
     try {
         const games = await connection.query(`
@@ -116,6 +123,98 @@ server.put('/customers/:id', async (req, res) => {
             name = $2, phone = $3, cpf = $4, birthday = $5
             WHERE customers.id = $1;
         `, [id, name, phone, cpf, birthday]);
+        res.sendStatus(200);
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500);
+    }
+});
+
+//RENTALS
+server.get('/rentals', async (req, res) => {
+    try {
+        const rentals = await connection.query(`
+        SELECT rentals.*,
+        customers.name AS "customerName",
+        games.name AS "gameName", games."categoryId",
+        categories.name AS "categoryName"
+        FROM rentals
+        JOIN customers
+        ON rentals."customerId" = customers.id
+        JOIN games
+        ON rentals."gameId" = games.id
+        JOIN categories
+        ON games."categoryId" = categories.id`);
+
+        res.send(rentals.rows.map(r => {
+            return {
+                id: r.id,
+                customerId: r.customerId,
+                gameId: r.gameId,
+                rentDate: r.rentDate,
+                daysRented: r.daysRented,
+                returnDate: r.returnDate, // troca pra uma data quando já devolvido
+                originalPrice: r.originalPrice,
+                delayFee: r.delayFee,
+                customer: {
+                    id: r.customerId,
+                    name: r.customerName
+                    },
+                game: {
+                    id: r.gameId,
+                    name: r.gameName,
+                    categoryId: r.categoryId,
+                    categoryName: r.categoryName
+                    }
+            }
+        }))
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500);
+    }
+});
+
+server.post('/rentals', async (req, res) => {
+    try {
+        const { customerId, gameId, daysRented } = req.body;
+
+        const pricePerDay = await connection.query(`
+        SELECT games."pricePerDay" 
+        FROM games WHERE games.id = $1
+        `, [gameId]);
+
+        const numberOfDays = parseInt(daysRented)*10;
+        const originalPrice = numberOfDays*pricePerDay.rows[0].pricePerDay;
+
+        await connection.query(`
+            INSERT INTO rentals 
+            ("customerId", "gameId", "daysRented", "rentDate", "originalPrice") 
+            VALUES ($1, $2, $3, $4, $5);
+        `, [customerId, gameId, daysRented, now, originalPrice]);
+        res.sendStatus(201);
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500);
+    }
+});
+
+server.post('/rentals/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const rentals = await connection.query(`
+            SELECT * FROM rentals WHERE rentals.id = $1;
+        `,[id]);
+        res.send(rentals.rows);
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500);
+    }
+});
+
+server.delete('/rentals/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        await connection.query(`DELETE FROM rentals WHERE rentals.id = $1`, [id]);
         res.sendStatus(200);
     } catch (err) {
         console.log(err)
